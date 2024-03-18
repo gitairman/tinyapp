0fa418216adc69a321ddd76293303567f8799722
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-18 08:12:54 -0700

    added ejs partial

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   b2xVn2: 'http: //www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
@@ -9,6 +11,17 @@ const urlDatabase = {
 
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:id', (req, res) => {
+  const id = req.params.id;
+  const templateVars = { id, longURL: urlDatabase[id] };
+  res.render('urls_show', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
