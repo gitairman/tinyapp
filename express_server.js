be68c9434d2a4c78cf378f94d13f3f396b204558
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-18 15:06:08 -0700

    added delete button

--- express_server.js
+++ express_server.js
@@ -82,6 +82,13 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${randStr}`);
 });
 
+app.post('/urls/:id/delete', (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
